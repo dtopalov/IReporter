ae63c49c1059ae18d6f9203ab6c08498ea682c92
dtopalov <dimiter.topalov79@gmail.com>
2016-02-14 17:16:12 +0200

    two user roles added

--- Data/IReporter.Data/Migrations/Configuration.cs
+++ Data/IReporter.Data/Migrations/Configuration.cs
@@ -22,6 +22,12 @@
             const string AdministratorUserName = "admin@admin.com";
             const string AdministratorPassword = AdministratorUserName;
 
+            const string RegularUserUserName = "user@user.com";
+            const string RegularUserPassword = RegularUserUserName;
+
+            const string AuthorUserName = "author@author.com";
+            const string AuthorPassword = AuthorUserName;
+
             if (!context.Roles.Any())
             {
                 // Create admin role
@@ -30,14 +36,28 @@
                 var role = new IdentityRole { Name = GlobalConstants.AdministratorRoleName };
                 roleManager.Create(role);
 
-                // Create admin user
+                // Create user role
+                role = new IdentityRole { Name = GlobalConstants.UserRoleName };
+                roleManager.Create(role);
+
+                // Create author role
+                role = new IdentityRole { Name = GlobalConstants.AuthorRoleName };
+                roleManager.Create(role);
+
+                // Create admin, author, and user
                 var userStore = new UserStore<ApplicationUser>(context);
                 var userManager = new UserManager<ApplicationUser>(userStore);
-                var user = new ApplicationUser { UserName = AdministratorUserName, Email = AdministratorUserName };
-                userManager.Create(user, AdministratorPassword);
+                var admin = new ApplicationUser { UserName = AdministratorUserName, Email = AdministratorUserName };
+                userManager.Create(admin, AdministratorPassword);
+                var author = new ApplicationUser { UserName = AuthorUserName, Email = AuthorUserName };
+                userManager.Create(author, AuthorPassword);
+                var regularUser = new ApplicationUser { UserName = RegularUserUserName, Email = RegularUserUserName };
+                userManager.Create(regularUser, RegularUserPassword);
 
-                // Assign user to admin role
-                userManager.AddToRole(user.Id, GlobalConstants.AdministratorRoleName);
+                // Assign users to roles
+                userManager.AddToRole(admin.Id, GlobalConstants.AdministratorRoleName);
+                userManager.AddToRole(author.Id, GlobalConstants.AuthorRoleName);
+                userManager.AddToRole(regularUser.Id, GlobalConstants.UserRoleName);
             }
         }
     }
--- IReporter.Common/GlobalConstants.cs
+++ IReporter.Common/GlobalConstants.cs
@@ -3,5 +3,9 @@
     public class GlobalConstants
     {
         public const string AdministratorRoleName = "Administrator";
+
+        public const string UserRoleName = "RegularUser";
+
+        public const string AuthorRoleName = "Author";
     }
 }
